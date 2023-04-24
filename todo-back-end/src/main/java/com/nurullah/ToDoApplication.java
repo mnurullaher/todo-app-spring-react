d4dc30f6dae8d1943c275ef1b4d8f4273f724f6b
mnurullaher <nurullah3080@gmail.com>
2023-04-24 15:35:16 +0300

    gitignore modified

--- todo-back-end/src/main/java/com/nurullah/ToDoApplication.java
+++ todo-back-end/src/main/java/com/nurullah/ToDoApplication.java
@@ -1,4 +1,4 @@
-package com.nurullah.ToDo;
+package com.nurullah;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- todo-back-end/src/test/java/com/nurullah/ToDoApplicationTests.java
+++ todo-back-end/src/test/java/com/nurullah/ToDoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nurullah.ToDo;
+package com.nurullah;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
