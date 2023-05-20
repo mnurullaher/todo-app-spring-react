179101723a45c5a94834a92100ba33febb8e9535
mnurullaher <mnurullaher@gmail.com>
2023-05-20 15:27:03 +0300

    validation added to login and signup forms

--- todo-front-end/src/pages/LoginPage.tsx
+++ todo-front-end/src/pages/LoginPage.tsx
@@ -1,13 +1,16 @@
 import { useContext, useState } from "react";
 import AuthContext from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
+import * as yup from "yup";
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
 
 export const LoginPage = () => {
-    
+
     const navigate = useNavigate()
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
-    let {loginUser}: any = useContext(AuthContext);
+    let { loginUser }: any = useContext(AuthContext);
 
     const login = async (e: any) => {
         e.preventDefault()
@@ -15,20 +18,35 @@ export const LoginPage = () => {
         navigate("/")
     }
 
+    const loginFormSchema = yup.object().shape({
+        username: yup.string().required("Username can't be empty"),
+        password: yup.string().required("Password can' be empty")
+    });
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(loginFormSchema)
+    })
+
+    const onSubmit = (data: any, e: any) => {
+        login(e);
+    }
+
     return (
         <div className="form-container">
-            <form onSubmit={login} className="form">
-                <input type="text" name="username" 
+            <form onSubmit={handleSubmit(onSubmit)} className="form">
+                <input type="text" {...register("username")} name="username"
                     value={username} onChange={(e) => setUsername(e.target.value)}
                     placeholder="Enter your username..." className="form-input" />
-                <input type="password" name="password"
+                <p className="error-message">{errors.username?.message?.toString()}</p>
+                <input type="password" {...register("password")} name="password"
                     value={password} onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter your password..." className="form-input"/>
+                    placeholder="Enter your password..." className="form-input" />
+                <p className="error-message">{errors.password?.message?.toString()}</p>
                 <button className="submit-btn" type="submit"> Login </button>
                 <span>Don't have an account..?</span><Link to={"/signup"} id="createAccountBtn">Create an account</Link>
             </form>
-            
+
         </div>
     );
-    
+
 }
--- todo-front-end/src/pages/SignupPage.tsx
+++ todo-front-end/src/pages/SignupPage.tsx
@@ -1,5 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { signupUser } from "../client/Auth";
+import * as yup from "yup";
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
 
 export const SignupPage = () => {
 
@@ -22,13 +25,30 @@ export const SignupPage = () => {
         }
     }
 
+    const signupFormSchema = yup.object().shape({
+        fullname: yup.string().required("Name can't be empty"),
+        username: yup.string().required("Username cant't be empty"),
+        password: yup.string().required("Password can't be empty")
+    })
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(signupFormSchema)
+    })
+
+    const onSubmit = (data: any, e: any) => {
+        addUser(e)
+    }
+
     return (
 
         <div className="form-container">
-            <form onSubmit={addUser} className="form">
-                <input type="text" name="fullname" placeholder="Enter your fullname.." className="form-input" />
-                <input type="text" name="username" placeholder="Enter your username..." className="form-input" />
-                <input type="password" name="password" placeholder="Enter your password..." className="form-input" />
+            <form onSubmit={handleSubmit(onSubmit)} className="form">
+                <input type="text" {...register("fullname")} name="fullname" placeholder="Enter your fullname.." className="form-input" />
+                <p className="error-message">{errors.fullname?.message?.toString()}</p>
+                <input type="text" {...register("username")} name="username" placeholder="Enter your username..." className="form-input" />
+                <p className="error-message">{errors.username?.message?.toString()}</p>
+                <input type="password" {...register("password")} name="password" placeholder="Enter your password..." className="form-input" />
+                <p className="error-message">{errors.password?.message?.toString()}</p>
                 <button className="submit-btn" type="submit"> Sign Up </button>
             </form>
         </div>
